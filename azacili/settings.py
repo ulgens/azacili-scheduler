6fc02a5c0de92e56716a70b1700ec85907d792b9
Ülgen Sarıkavak <ulgensrkvk@gmail.com>
2018-08-26 18:17:03 +0300

    Enhance models

--- azacili/settings.py
+++ azacili/settings.py
@@ -31,12 +31,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Django
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Apps
+    "accounts",
+    "schedule",
 ]
 
 MIDDLEWARE = [
@@ -98,6 +103,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+AUTH_USER_MODEL = "accounts.User"
 
 
 # Internationalization
--- schedule/models.py
+++ schedule/models.py
@@ -1,7 +1,19 @@
 from django.db import models
+from django.utils.translation import ugettext as _
 
 
-class Lecturer(models.Model):
+DAY_OF_THE_WEEK = (
+    (1, _('Monday')),
+    (2, _('Tuesday')),
+    (3, _('Wednesday')),
+    (4, _('Thursday')),
+    (5, _('Friday')),
+    (6, _('Saturday')),
+    (7, _('Sunday')),
+)
+
+
+class Instructor(models.Model):
     name = models.CharField(max_length=128)
 
     def __str__(self):
@@ -28,9 +40,25 @@ class Course(models.Model):
     name = models.CharField(max_length=32)
     program = models.ForeignKey(Program, on_delete=models.PROTECT)
 
+    def __str__(self):
+        return self.name
+
 
 class Section(models.Model):
-    code = models.IntegerField()
+    code = models.IntegerField(verbose_name="CRN")
     course = models.ForeignKey(Course, on_delete=models.PROTECT)
-    lecturer = models.ForeignKey(Lecturer, on_delete=models.PROTECT)
+    lecturer = models.ForeignKey(Instructor, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.code
+
+
+class Lesson(models.Model):
     building = models.ForeignKey(Building, on_delete=models.PROTECT)
+    room = models.CharField(max_length=16)
+    day = models.PositiveSmallIntegerField(choices=DAY_OF_THE_WEEK)
+    start_time = models.TimeField(verbose_name="Ders başlangıç saati")
+    end_time = models.TimeField(verbose_name="Ders bitiş saati")
+
+    def __str__(self):
+        return f"{self.get_day_display()}: {self.start_time} - {self.end_time}"
