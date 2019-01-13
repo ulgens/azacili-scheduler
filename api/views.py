587f25143895290e1eff260a843c2a1a7f3c86e2
Ülgen Sarıkavak <ulgensrkvk@gmail.com>
2019-01-13 10:55:20 +0300

    Small efforts to optimize query performance.

--- api/views.py
+++ api/views.py
@@ -25,7 +25,15 @@ class CourseViewset(ReadOnlyModelViewSet):
     @action(detail=True)
     def sections(self, request, pk=None):
         course = self.get_object()
-        serializer = SectionSerializer(course.section_set.all(), many=True)
+        serializer = SectionSerializer(
+            course.section_set.all().select_related(
+                "lecturer",
+            ).prefetch_related(
+                "lesson_set",
+                "lesson_set__building",
+            ),
+            many=True,
+        )
 
         return Response(serializer.data)
 
