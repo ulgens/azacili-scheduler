19c9a73bdc26c7fd8e7f88a57db7e746a4a86e5e
Ülgen Sarıkavak <ulgensrkvk@gmail.com>
2019-06-10 09:27:15 +0300

    Fix typo in term name

--- schedule/utils.py
+++ schedule/utils.py
@@ -117,7 +117,7 @@ def update_courses(programs=None):
             course, course_created = Course.objects.get_or_create(
                 code=course_code,
                 program=program,
-                term="2018-2109-03",
+                term="2018-2019-03",
                 defaults={"name": course_name},
             )
 
